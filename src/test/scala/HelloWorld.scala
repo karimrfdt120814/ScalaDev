019e9ac8053f6774ee57d08c946041dfdbcefdaa
Karimulla <karimrfdt120814@gmail.com>
2023-07-16 19:16:08 +0530

    Conditional statements and looping statements.

--- src/test/scala/HelloWorld.scala
+++ src/test/scala/HelloWorld.scala
@@ -4,25 +4,10 @@ object HelloWorld {
 
   object persistdemo {
     def main(args: Array[String]): Unit = {
-      val sc = new SparkContext("local[*]", "persistdemo");
-
-
-      // Create RDD
-      val rdd = sc.parallelize(1 to 10000000)
-
-      // Calculate the sum of all elements in the RDD without persisting
-      val sum1 = rdd.reduce(_ + _)
-      println("Sum of RDD without persisting: " + sum1)
-
-      // Calculate the sum of all elements in the RDD after persisting
-      rdd.persist()
-      val sum2 = rdd.reduce(_ + _)
-      println("Sum of RDD with persisting: " + sum2)
-
-      // Unpersist RDD from memory
-      rdd.unpersist()
-
-      scala.io.StdIn.readLine()
+      val name = "Alice"
+      val age = 30
+      val message = s"My name is $name and I'm $age years old."
+      println(message) // Output: My name is Alice and I'm 30 years old
     }
 
   }
