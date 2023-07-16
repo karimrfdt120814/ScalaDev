6be208dd5f6b3dcbef6e0b8858fe8ce0f5525377
Karimulla <karimrfdt120814@gmail.com>
2023-07-16 20:05:10 +0530

    Conditional statements and looping statements.

--- src/main/scala/DataTypes.scala
+++ src/main/scala/DataTypes.scala
@@ -1,13 +1,15 @@
+
+
 object DataTypes {
 
   def main(args: Array[String]): Unit = {
 
-   /* val name = "Bob"
+    /* val name = "Bob"
     val height = 1.85
     val message = f"Hello, $name. Your height is $height%2.2f meters."
     println(message) // Output: Hello, Bob. Your height is 1.85 meters. */
 
-   /* val a =25
+    /* val a =25
     val b = 30
     val c= 35
 
@@ -22,18 +24,31 @@ object DataTypes {
         print(s"$c is smaller number")
       } */
 
-    val range = 1 to 10;
+    /* val range = 1 to 100;
 
-    val a = 1 until 10;
+    val a = 1 until 100;
 
 
     for (i <- a) {
-      println(i)
-    }
 
+      if (i%2 == 0) {
+        println(i)
+      }
+    } */
 
+   /* var a = 1
+    while (a <= 100) {
+      if (a % 2 == 0) {
+        println(a)
+        a = a + 2
+      }
+      a = a + 1
 
-
+    } */
   }
+
+
+
+
 
 }
